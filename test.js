6cd231919c298d56594449a538e8f5d5148f872a
Travis Webb <me@traviswebb.com>
2014-09-23 17:09:24 -0400

    added account orm test

--- test.js
+++ test.js
@@ -18,16 +18,22 @@ describe('waterline-discovery-doc-import', function () {
   };
 
   describe('#createCollections', function () {
+    var orm;
+
     it('should create a valid waterline orm', function (done) {
       this.timeout(60 * 1000);
       var waterline = new Waterline();
       var collections = WaterlineDiscovery.createCollections(fixtures.xtuple460);
       _.each(collections, waterline.loadCollection, waterline);
 
-      waterline.initialize(config, function (error, orm) {
-        console.log(orm);
+      waterline.initialize(config, function (error, _orm) {
+        orm = _orm.collections;
         done(error);
       });
+    });
+
+    it('should contain an Account orm model for xtuple fixture test', function () {
+      assert(_.isObject(orm.account));
     });
   });
 });
